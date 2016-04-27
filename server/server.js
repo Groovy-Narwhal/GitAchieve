f9da23d94cb572d4c043531aa59727ed875a4acf
byeo630 <byeo630@gmail.com>
2016-04-27 16:41:54 -0700

    (feat) Create server

--- server/server.js
+++ server/server.js
@@ -4,26 +4,28 @@ var config = require('../webpack.config.js');
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
+var bodyParser = require('body-parser');
+var passport = require('passport');
+var passportGH = require('passport-github');
+var morgan = require('morgan');
+var logger = require('logger');
+var fs = require('fs');
 
+// Initiate server
 var app = express();
-
 var compiler = webpack(config);
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
-
 app.use(express.static('./dist'));
-
 app.use('/', function(req, res) {
   res.sendFile(path.resolve('client/index.html'));
 });
 
-var port = 3000;
+const port = process.env.PORT || 8000;
+console.log('Listening in on ', port);
+app.listen(port);
 
-app.listen(port, function(error) {
-  if (error) {
-    throw err;
-  } else {
-    console.log('app listening on port ', port);
-  }
-})
+module.exports = app;
