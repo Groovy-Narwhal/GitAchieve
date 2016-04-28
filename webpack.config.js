b7bd0742c90097e8391c7ee08796343af09a5501
Megan Smith <megan@tokaypress.com>
2016-04-27 22:55:45 -0700

    (feat) Set up webpack middleware in server and in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+var webpack = require('webpack');
+
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
-  entry: ['./client/index.js'],
+  entry: [
+    'webpack-hot-middleware/client',
+    './client/index.js'
+  ],
   output: {
-    path: './dist',
+    path: require('path').resolve('./dist'),
     filename: 'bundle.js',
     publicPath: '/'
   },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {
