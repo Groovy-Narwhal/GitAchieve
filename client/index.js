31c0928abf1fb571226b6264c6479817b5ef285b
Megan Smith <megan@tokaypress.com>
2016-04-29 10:44:53 -0700

    (feat) Start implementing react router

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore, routerReducer } from 'react-router-redux';
 import App from './containers/app';
 import configureStore from './store/store';
 
@@ -14,7 +16,27 @@ let store = configureStore(initialState);
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path='/' component={App}>
+        <Route path="v1/users" component={User} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
+
+
+
+// ReactDOM.render(
+//   <Router history={browserHistory}>
+//     <Route path="/events" component={App}>
+//       <IndexRoute component={AddEventForm} />
+//       <Route path="/events/:eventName" component={EventPage} />
+//     </Route>
+//   </Router>,
+//   document.getElementById('app')
+// );
+
+// App.propTypes = {
+//   children: React.PropTypes.object,
+// };
