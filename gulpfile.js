a5a7073cd312bc36b0d4d864790ba1e2bf78a92e
byeo630 <byeo630@gmail.com>
2016-04-29 19:41:20 -0700

    (feat) Add gulp file and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,29 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
+var util = require('gulp-util');
+var webpack = require('webpack-stream');
+var mochaPhantomJs = require('gulp-mocha-phantomjs');
 
 // Lint Task
 gulp.task('lint', function() {
     return gulp.src('./*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+});
+
+// Test Task
+gulp.task('test', function() {
+  return gulp.src('test/client/index.html')
+    .pipe(mochaPhantomJs());
+});
+
+// Build Task
+gulp.task('build', function() {
+  return gulp.src('client/index.js')
+    .pipe(webpack())
+    .pipe(gulp.dest('dist/'));
 });
 
 // Compile Our Sass // Commented out for now since Sass is not integrated yet.
@@ -22,7 +39,7 @@ gulp.task('lint', function() {
 //         .pipe(gulp.dest('dist/css'));
 // });
 
-// Concatenate & Minify JS
+// Concatenate & Minify JS Task
 gulp.task('scripts', function() {
     return gulp.src('server/**/*.js')
         .pipe(concat('all.js'))
@@ -34,10 +51,9 @@ gulp.task('scripts', function() {
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('./*.js', ['lint', 'scripts']);
+    gulp.watch(['./*.js', 'test/**'], ['lint', 'scripts', 'test']);
     // gulp.watch('scss/*.scss', ['sass']);
 });
 
 // Default Task
-// gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
 gulp.task('default', ['lint', 'scripts', 'watch']);
---
+++ test/client/index.js
@@ -0,0 +1,10 @@
+
+
+describe('test setup', function() {
+  it('should work', function() {
+    expect(true).toBe.true;
+  });
+  it('should do math', function() {
+    expect(2 + 2).toBe(4);
+  });
+});
