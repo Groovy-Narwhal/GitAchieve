ecbebaf72e68bbf82bd5ae9e196289de99e2e117
byeo630 <byeo630@gmail.com>
2016-05-03 16:11:49 -0700

    (feat) Add number ticks and days of the week to commit graph

--- client/components/dashBoard.js
+++ client/components/dashBoard.js
@@ -43,24 +43,25 @@ const UserCommitChart = () => {
   var commitCounts = svg.append('g')
       .attr('class', 'commitCounts');
 
+  var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
   // TODO: take out hardcoded value and replace it with a variable
   d3.json("https://api.github.com/repos/Groovy-Narwhal/GitAchieve/stats/commit_activity", (error, data) => {
     const currentWeekCommits = data[data.length - 1];
     x.domain([0, 7]);
     y.domain([0, d3.max(currentWeekCommits.days, (d) => { return d; })]);
-    let i = 0;
     if (error) {
       console.log('There was an error retrieving commit data: ', error);
     }
     var commitsMax = d3.max(currentWeekCommits.days)
+    let i = 0;
     var commitCount = commitCounts.selectAll('.commit-count')
         .data(currentWeekCommits.days)
       .enter().append('g')
         .attr('class', 'commit-count')
         .attr('transform', (commitCount) => (`translate(${x(i++)}, 0)`));
-
     i = 0;
-    
+
     commitCount.selectAll('rect')
         .data((d) => { return [d]})
       .enter().append('rect')
@@ -70,6 +71,19 @@ const UserCommitChart = () => {
         .attr('height', (value) => { console.log('The height value: ', height - y(value)); return height - y(value) })
         .text((d) => (d.toString()));
 
+    commitCount.append('text')
+        .attr('y', height - 4)
+        .attr('class', 'commit-count-num')
+        .text((commits) => (commits));
+
+    svg.selectAll('.day')
+        .data(week)
+      .enter().append('text')
+        .attr('class', 'day')
+        .attr('x', () => (x(i++) + 3))
+        .attr('text-anchor', 'middle')
+        .attr('y', height + 20)
+        .text((day) => ( day ))
     i = 0;
   });
 };
