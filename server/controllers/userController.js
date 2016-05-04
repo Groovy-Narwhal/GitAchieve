16154c023685d36d226353c3c1458ab151323a10
Alex Nitta <a.j.nitta@gmail.com>
2016-05-03 19:32:05 -0700

    Improve error handling on GET, POST and PATCH at /api/v1/users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,11 +2,12 @@ const db = require('../db/database.js');
 
 // GET at /api/v1/users
 exports.retrieve = function(req, res) {
-  db.run('SELECT * FROM users', function(error, response) {
-    if (error) {
-      console.error(error);
+  db.run('SELECT * FROM users', function(err, data) {
+    if (err) {
+      res.status(500).send(err);
+      console.error(err);
     } else {
-      res.send(response);
+      res.send(data);
     }
   }); 
 };
@@ -14,13 +15,14 @@ exports.retrieve = function(req, res) {
 // POST at /api/v1/users
 exports.addOne = function(req, res) {  
   var timestamp = new Date();
-  db.run('INSERT INTO users (username, email, id, created_ga) VALUES ($1, $2, $3, $4)', [req.body.username, req.body.email, req.body.id, timestamp], function(error, response) {
-    if (error) {
-      console.error(error);
+  db.run('INSERT INTO users (username, email, id, created_ga) VALUES ($1, $2, $3, $4)', [req.body.username, req.body.email, req.body.id, timestamp], function(err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
     } else {
-      var message = 'User created with username: ' + req.body.username + ' and email: ' + req.body.id;
+      console.log('User created with username: ' + req.body.username + ' and email: ' + req.body.id);
       res.status(201);
-      res.send(message);
+      res.send(req.body);
     }
   });
 };
@@ -28,11 +30,19 @@ exports.addOne = function(req, res) {
 // GET at '/api/v1/users/:id'
 exports.retrieveOne = function(req, res) {
   var queryId = req.params.id;
-  db.run('SELECT * FROM users WHERE id=($1)', [queryId], function(error, user) {
-    if (error) {
-      console.error(error);
+  db.run('SELECT * FROM users WHERE id=($1)', [queryId], function(err, data) {
+    if (err) {
+      console.error(err);
+      res.status(500).send(err);
     } else {
-      res.send(user);
+      // if there is no matching user
+      if (data.length === 0) {
+        // send 404
+        res.send(404);
+      } else {
+        // otherwise, send matching user
+        res.send(data);
+      }
     }    
   });
 };
@@ -40,33 +50,55 @@ exports.retrieveOne = function(req, res) {
 // PATCH at '/api/v1/users/:id'
 exports.updateOne = function(req, res) {
   var queryId = req.params.id;
-  var keyCount = 0;
-  for (var key in req.body) {
-    keyCount++;
-  }
-  db.run('SELECT * FROM users WHERE id=($1)', [queryId], function(error, user) {
-    if (error) {
-      console.error(error);
+
+  // first, find the user by id
+  db.run('SELECT * FROM users WHERE id=($1)', [queryId], function(err, data) {
+    if (err) {
+      console.error(err.message);
+      res.status(500).send(err);
     } else {
-      if (user.length === 0) {
-        res.status(404).send();
-      } else {
-        var updatedUser = user[0];
+      // if there are no matching users,
+      if (data.length === 0) {
+        // send 404
+        res.send(404);
+      } else { // if there is a matching user,
+        // set up keyCount to determine when to send the full response
+        var keyCount = 0;
+        // check every key in the request body
         for (var key in req.body) {
+          // if the user does not have that property
+          if (data[0][key] === undefined) {
+            // send 500
+            res.status(500).send('You are trying to update the user.' + key + ' property, which doesn\'t exist');
+          } else {
+            // otherwise, increment key count
+            keyCount++;
+          }
+        }
+        // copy user into an object called updatedUser
+        var updatedUser = data[0];
+        // for each key:value pair in the request body
+        for (var key in req.body) {
+          // update the updatedUser
           updatedUser[key] = req.body[key];
-          db.run('UPDATE users SET ' + key + '=($1) WHERE id=($2)', [req.body[key], queryId], function (err, user) {
+          // and update the database
+          db.run('UPDATE users SET ' + key + '=($1) WHERE id=($2)', [req.body[key], queryId], function (err, data) {
             if (err) {
-              console.error(err);
+              console.error(err.message);
+              res.status(500).send(err);
             } else {
+              // each time a key is added to the database, decrement keyCount
               keyCount --;
+              // when there are no more keys left
               if (keyCount === 0) {
+                // send the updated user back
                 res.send(updatedUser);
               }
             }
           });
         }
       }
-    }    
+    }
   });
 };
 
