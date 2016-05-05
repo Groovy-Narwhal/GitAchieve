c1d58c8255306563a9b5945d4107c667105cdd03
Megan Smith <megan@tokaypress.com>
2016-05-04 19:44:22 -0700

    (feat) Start Working on testing

--- test/client/index.js
+++ test/client/index.js
@@ -1,8 +1,54 @@
 import React from 'react';
-import { expect } from 'chai';
-import { shallow, mount, render } from 'enzyme';
-// TODO: import React and create tests for the front end
-import App from '../../client/components/app.js';
+import expect from 'expect';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import nock from 'nock';
+// import chai, { expect } from 'chai';
+// import { shallow, mount, render } from 'enzyme';
+// import App from '../../client/components/app.js';
+import * as actions from '../../client/actions/ActionCreators';
+import * as asyncActions from '../../client/actions/index';
+import * as types from '../../client/actions/actionTypes';
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares);
+
+describe('actions', () => {
+  it('should create an action to authorize a user', () => {
+    const expectedAction = {
+      type: types.AUTH_USER
+    }
+    expect(actions.default.authUser()).toEqual(expectedAction);
+  });
+  it('should create an action to unauthorize a user', () => {
+    const expectedAction = {
+      type: types.UNAUTH_USER
+    }
+    expect(actions.default.unAuthUser()).toEqual(expectedAction);
+  });
+});
+
+describe('async actions', () => {
+  afterEach(() => {
+    nock.cleanAll()
+  })
+  it('creates ', () => {
+    nock('http://example.com')
+      .get('/')
+      .reply(200, { body: { todos: ['do something'] }})
+      const expectedActions = [
+        { type: types.UNAUTH_USER }
+      ]
+      const store = mockStore({
+        auth: {
+          authorized: true
+        }
+      });
+  });
+});
+
+
+
 // TODO: load in the database and test
   // Connect to Postgres
 
@@ -18,14 +64,14 @@ For testing resources check these:
 ********************************************************/
 
 // Test if Mocha Chai works
-describe('test setup', function() {
-  it('should work', function() {
-    expect(true).to.be.true;
-  });
-  it('should do math', function() {
-    expect(2 + 2).to.eql(4);
-  });
-});
+// describe('test setup', function() {
+//   it('should work', function() {
+//     expect(true).to.be.true;
+//   });
+//   it('should do math', function() {
+//     expect(2 + 2).to.eql(4);
+//   });
+// });
 
 // if logging in works
 // components exist
@@ -34,13 +80,13 @@ describe('test setup', function() {
 // we can access the state from a certain component
 
 /* Front-end Tests */
-describe('load app component', function() {
-  it('should load react app', () => {
-    // const wrapper = mount(<App />);
-    // expect(App).to.exist;
-  })
-  // TODO: write tests here.
-});
+// describe('load app component', function() {
+//   it('should load react app', () => {
+//     const wrapper = mount(<App />);
+//     expect(App).to.exist;
+//   })
+//   // TODO: write tests here.
+// });
 
 /********************************************************
 // Test if it loads React
