b3c10a9ff3a179dbc94717501b7e01fca972c6bd
adamrgisom <adamrgisom@gmail.com>
2016-05-05 14:36:30 -0700

    Halfway to refactoring shape of the data in commitChart

--- client/components/commitChart.js
+++ client/components/commitChart.js
@@ -1,6 +1,7 @@
 //@TODO: take out hardcoded value in d3.json and replace it with a variable
 //@TODO: integrate with a style sheet (if desired at that point edit .axis text to have better font)
 //@TODO: make the 'week' array depend on the data received / day-of-the-week that the API request was made
+//@TODO: make sure we don't get an error relating to the d3.json having not returned
 
 exports.CommitChart = () => {
 
@@ -10,26 +11,84 @@ exports.CommitChart = () => {
     if (error) {
       console.log('There was an error retrieving commit data: ', error);
     }
+
+    /*
+    Let's think about the shape of our data.
+    We want highest # of commits overall, to properly scale the y axis.
+    We also want the highest # of commits on a particular day, to draw the overlapping rectangles in the right order.
+    In the FUTURE, if we get a third (or more) set of data, we actually want to switch the mode of our D3 graph!
+    (From overlapping bars to side-by-side skinny bars.)
+
+    The shape of our data suggests an array of arrays so that's what we'll do.
+    */
+
+    // Our user's set of data -- unsure how to add a second set in right now
+    var userCommits = data[data.length - 1];
+    var userCommitsTotal = userCommits.total;
+
+    // Dummy second set of data:
+    var dummy = {total: 35, days: [7, 3, 22, 2, 0, 3, 3]};
+    var dummyTotal = dummy.total;
+
+    // Days of the week are used in the x-scaling and x-axis
+    // We'll want to dynamically generate this from the data in the future
+    var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+    // Now we can process all our data into one matrix
+    var mostCommits = 0;
+    var matrix = [];
+    for (var i = 0; i < 7; i++) {
+      matrix.push(
+      [
+        week[i],
+        userCommits.days[i],
+        dummy.days[i]
+      ]);
+    }
+    for (var j = 0; j < 7; j++) {
+      var indexOfHighestOnDay = -1;
+      for (var k = 1; k < matrix[0].length; k++) {
+        if (matrix[j][k] > mostCommits)
+          mostCommits = matrix[j][k];
+        if (matrix[j][k] > highestOnDay)
+          indexOfHighestOnDay = k;
+      }
+      matrix[j].push(k); // theoretically this is the index of the user with the most commits for a particular day
+    }
+
+
+
     // Data = all of the weeks of the past year and how many commits are in each week
     // set placeholders for received data
-    var currentWeekCommits;
-    var commitsPairedWithDays = [];
-    // Grab the last week's commit data
-    // It looks like this: {total: 35, days: [5, 9, 19, 2, 0, 0, 0]}
-    // days of the week will be used for the graph's x-axis
-    var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    currentWeekCommits = data[data.length - 1];
-    for (var i = 0; i < 7; i++) {
-      commitsPairedWithDays.push(
-      {
-        day: week[i],
-        user: currentWeekCommits.days[i],
-        hardcodedUserTwo: 1
-      });
-    }
-    // OVERWRITE FOR PURPOSES OF PRACTICE
-    commitsPairedWithDays[2].hardcodedUserTwo = currentWeekCommits.days[2]+2;
-    commitsPairedWithDays[5].hardcodedUserTwo = currentWeekCommits.days[5]+2;
+    // var currentWeekCommits;
+    // var commitsPairedWithDays = [];
+    // var mostCommits = 0;
+    // // Grab the last week's commit data
+    // // It looks like this: {total: 35, days: [5, 9, 19, 2, 0, 0, 0]}
+    // // days of the week will be used for the graph's x-axis
+    // currentWeekCommits = data[data.length - 1];
+
+    // // NOTE THE HARDCODED DATA AT PROPERTY HARDCODEDUSERTWO
+    // for (var i = 0; i < 7; i++) {
+    //   commitsPairedWithDays.push(
+    //   {
+    //     day: week[i],
+    //     user: currentWeekCommits.days[i],
+    //     hardcodedUserTwo: 1
+    //   });
+    // }
+    // // OVERWRITE FOR PURPOSES OF PRACTICE
+    // commitsPairedWithDays[2].hardcodedUserTwo = currentWeekCommits.days[2]+2;
+    // commitsPairedWithDays[5].hardcodedUserTwo = currentWeekCommits.days[5]+2;
+
+    // // FIND HIGHEST NUMBER OF COMMITS
+    // var mostCommits = 0;
+    // for (var i = 0; i < commitsPairedWithDays.length; i++) {
+    //   if (commitsPairedWithDays[i].user > mostCommits)  mostCommits = commitsPairedWithDays[i].user;
+    //   if (commitsPairedWithDays[i].hardcodedUserTwo > mostCommits)  mostCommits = commitsPairedWithDays[i].hardcodedUserTwo;
+    // }
+
+
 
     // set dimensions
     var pad = 20;
@@ -48,12 +107,6 @@ exports.CommitChart = () => {
     var xScale = d3.scale.ordinal()
       .domain(week)
       .rangeRoundBands( [pad*2, w] );
-    // Find the highest number of commits for any user being compared to set y's max
-    var mostCommits = 0;
-    for (var i = 0; i < commitsPairedWithDays.length; i++) {
-      if (commitsPairedWithDays[i].user > mostCommits)  mostCommits = commitsPairedWithDays[i].user;
-      if (commitsPairedWithDays[i].user > mostCommits)  mostCommits = commitsPairedWithDays[i].user;
-    }
     var yScale = d3.scale.linear()
       .domain(
         [mostCommits, 0])
@@ -82,12 +135,15 @@ exports.CommitChart = () => {
       .call(yAxis);
 
     // the stage is set -- add the data
+        // .attr('opacity', function(d) { return (d.user > d.hardcodedUserTwo ? 0.5 : 1); })
+    // make the larger bar be translucent (opacity = 0.5)
     var g = svg.selectAll(".bars")
         .data(commitsPairedWithDays)
-        .enter().append("g")
+        .enter()
+          .append("g")
       // place the first bar
       g.append("rect")
-        .style({ 'fill' : 'steelblue'})
+        .style({ 'fill' : 'steelblue' })
         .attr('x', (d) => xScale(d.day) )
         .attr('y', (d) => yScale(d.user) )
         .attr('width', barWidth )
