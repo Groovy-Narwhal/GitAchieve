6c375215b0d135ee9bb1366bc346c2f3e70fc6df
byeo630 <byeo630@gmail.com>
2016-05-07 16:56:13 -0700

    (feat) Add styling

--- client/components/headerProfileButton.js
+++ client/components/headerProfileButton.js
@@ -13,6 +13,26 @@ class HeaderUserButton extends Component {
     console.log('Initial open state: ', this.state.open);
   }
 
+  /* Only have the click events enabled when the menu is open */
+  componentDidUpdate(prevProps, prevState) {
+    if(this.props.open && !prevProps.open) {
+      console.log('previous open: ', prevProps.open, 'current open: ', this.props.open)
+      window.addEventListener('click', this.handleClickOutside.bind(this));
+    } else if(!this.props.open && prevProps.open) {
+      console.log('looptydoo');
+      window.removeEventListener('click', this.handleClickOutside.bind(this));
+    }
+  }
+
+  /* If clicked element is not in the dropdown menu children, close menu */
+  handleClickOutside(e) {
+    var children = ReactDOM.findDOMNode(this).getElementsByTagName('*');
+    for(var x in children) {
+      if(children[x] === e.target) { return; }
+    }
+    this.props.close(e);
+  }
+
   toggle() {
     this.setState({open: !this.state.open})
     console.log(this.state.open);
@@ -25,9 +45,12 @@ class HeaderUserButton extends Component {
 
   render() {
     return (
-      <div className="header-user-button float-right">
-        <img className="user-avatar-sm" onClick={this.toggle.bind(this)} src={this.props.user.avatar_url} />
-        {this.state.open ? <HeaderProfileDropdown open={this.state.open} forceCloseFunction={this.close.bind(this)} /> : null}
+      <div>
+        <div className="header-user-button float-right" onClick={this.toggle.bind(this)}>
+          <img className="user-avatar-sm" src={this.props.user.avatar_url} />
+          <img className="drop-arrow" src={'static/assets/dropArrow.png'} />
+          {this.state.open ? <HeaderProfileDropdown open={this.state.open} /> : null}
+        </div>
       </div>
     )
   }
--- client/components/headerProfileDropdown.js
+++ client/components/headerProfileDropdown.js
@@ -9,25 +9,6 @@ class HeaderUserButton extends Component {
     super(props)
   }
 
-  /* Only have the click events enabled when the menu is open */
-  // componentDidUpdate(prevProps, prevState) {
-  //   if(this.props.open && !prevProps.open) {
-  //     console.log('previous open: ', prevProps.open, 'current open: ', this.props.open)
-  //     window.addEventListener('click', this.handleClickOutside.bind(this));
-  //   } else if(!this.props.open && prevProps.open) {
-  //     window.removeEventListener('click', this.handleClickOutside.bind(this));
-  //   }
-  // }
-
-  /* If clicked element is not in the dropdown menu children, close menu */
-  handleClickOutside(e) {
-    var children = ReactDOM.findDOMNode(this).getElementsByTagName('*');
-    for(var x in children) {
-      if(children[x] === e.target) { return; }
-    }
-    this.props.forceCloseFunction(e);
-  }
-
   render() {
     return (
       <div className="header-profile-dropdown">
