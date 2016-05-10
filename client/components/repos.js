4035913b73003c3cfb8453235f48eeb447bd9d9f
byeo630 <byeo630@gmail.com>
2016-05-10 15:07:54 -0700

    (feat) Add loading status when repos are being fetched

--- client/components/repos.js
+++ client/components/repos.js
@@ -22,21 +22,8 @@ class Repos extends Component {
         }
       }
     }
+    // Initiates fetching of all repo data for the user
     this.fetchAllRepoData.call(this)
-    // this.getAllRepoData();
-  }
-  renderTopContributors(i) {
-    // console.log('Were inside of the renderTopContributors, ', i, this.state.reposContributors[i]);
-    // console.log('And here are the reposContributors: ', this.state.reposContributors);
-    return (
-      this.state.reposContributors[i].map((contributor) => (
-        <div>
-          <p>{contributor.login}</p>
-          <strong>Number of Contributions</strong>
-          <p>{contributor.contributions}</p>
-        </div>
-      ))
-    )
   }
   fetchRepos() {
     return fetch(`https://api.github.com/users/${this.props.user.username}/repos?per_page=100`, this.state.options)
@@ -48,102 +35,49 @@ class Repos extends Component {
       .then((res) => res.json())
       .then((data) => data);
   }
-  fetchAllRepoData(cb) {
+  fetchAllRepoData() {
     async function renderRepos () {
       var repos = await this.fetchRepos();
       var contributors = [];
       for (var i = 0; i < repos.length; i++) {
-        contributors.push(await this.fetchContributors(repos[i]))
+        contributors.push(await this.fetchContributors(repos[i]));
       }
-      // console.log('These are the repos: ', repos);
-      // console.log('These are the contributors: ', contributors);
       var incrementor = 0;
       var reposAndContributors = repos.map((repo) => ({repo: repo, contributors: contributors[incrementor++]}))
       this.setState({repos: reposAndContributors});
-      console.log('this.state.repos: ', this.state.repos);
     };
     renderRepos.call(this);
   }
   render() {
-    return (
-      <div id="data-results-container">
-        <h3>Repos</h3>
-        {this.state.repos.map((repoData) => (
-          <div className="data-result-container">
-            <h2>{repoData.repo.name}</h2>
-            {repoData.contributors.map((contributor) => (
-              <div>
-                <img src={contributor.avatar_url} className="user-avatar-med" />
-                <strong>{contributor.login}</strong>
-                <p>contributions: {contributor.contributions}</p>
-              </div>
+    if (this.state.repos.length === 0) {
+      var loadingMessages = ['Baking cupcakes...', 'Riding a narwhal...', 'Getting your groove on...'];
+      return (
+        <div>
+          <h2>{loadingMessages[Math.floor(Math.random() * loadingMessages.length)]}</h2>
+          <img src="../static/assets/loading.gif" />
+        </div>
+      );
+    } else {
+      return (
+        <div id="data-results-container">
+          <h3>Repos</h3>
+          {this.state.repos.map((repoData, i) => (
+            <div className="data-result-container" key={i} >
+              <h2>{repoData.repo.name}</h2>
+              {repoData.contributors.map((contributor, i) => (
+                <div key={i}>
+                  <img src={contributor.avatar_url} className="user-avatar-med" />
+                  <strong>{contributor.login}</strong>
+                  <p>contributions: {contributor.contributions}</p>
+                </div>
+              ))}
+            </div>
             ))}
-          </div>
-          ))}
-      </div>
-    )
+        </div>
+      );
+    }
   }
 }
-/*
-
-getAllRepoData() {
-  var initRepoFetch = () => {
-    return fetch(`https://api.github.com/users/${this.props.user.username}/repos?per_page=100`, this.state.options)
-      .then((res) => res.json())
-      .then(getRepoCommits)
-      .catch((err) => console.log('There was an error in initRepoFetch', err));
-  };
-  var getRepoCommits = (repos) => {
-    this.setState({repos: repos});
-    for (var i = 0; i < repos.length; i++) {
-      var repoName = repos[i].name;
-      var newReposContrState = {...this.state.reposContributors}
-      fetch(repos[i].contributors_url, this.state.options)
-        .then((res) => res.json())
-        .then((data) => {this.setState({reposContributors: newReposContrState[repoName] = data})})
-        .catch((err) => console.log(err));
-    }
-  };
-  initRepoFetch();
-}
-
-
-{this.state.repos.map((val, i) => {
-  console.log('This is the reposContributors and valname in line 66', this.state.reposContributors[`${val.name}`]);
-  console.log('This is the reposContributors in line 67', this.state.reposContributors);
-  return (
-  <div key={i} className="data-result-container">
-    <h2>{val.name}</h2>
-    <h3>score: {this.state.reposNumCommits[i]}</h3>
-    <strong>top contributors</strong>
-    {this.state.reposContributors[i] === undefined ? null : this.state.reposContributors[`${val.name}`].map((contributor, i) => {
-      if (i < 6) {
-        return (
-          <div key={i}>
-            <img src={contributor.avatar_url} className="user-avatar-med" />
-            <h2>{contributor.login}</h2>
-            <strong>Number of Contributions</strong>
-            <p>{contributor.contributions}</p>
-          </div>
-      )}}
-    )}
-  </div>
-  )
-})}
-
-*/
-
-// {this.renderTopContributors(i)}
-
-// {this.state.reposCommits.map((commits, i) => {
-//               console.log('Commit and i', commits, i);
-//               return (
-//               <div key={i} className="commits">
-//                 <strong>{commits[0].commit.author.name}</strong>
-//                 <p>{commits[0].commit.message}</p>
-//               </div>
-//               );
-//             })}
 
 const mapStateToProps = (state) => {
   return state;
