4f5070e335f86ab5f35803468123481b612cfedf
Alex Nitta <a.j.nitta@gmail.com>
2016-05-11 11:06:21 -0700

    (Feat) Work on statController

--- server/controllers/statController.js
+++ server/controllers/statController.js
@@ -15,20 +15,19 @@ exports.addStats = function(req, res) {
 };
 
 // PATCH at '/api/v1/user/:id/stats' to update stats 
-
 exports.updateStats = function(req, res) {
   var queryId = req.params.id;
   
   // ** HELPER FUNCTIONS **
   
-  //  get the stats for a given user from GitHub
-  var getStatsFromGitHub = function(owner, repo, callback) {
+  //  get the stats for a given owner & repo from GitHub
+  var getStatsFromGitHub = function(orgname, repo, callback) {
     var options = {
-      url: 'https://api.github.com/repos/' + owner.name + '/' + repo.name + '/stats/contributors',
+      url: 'https://api.github.com/repos/' + orgname + '/' + repo.name + '/stats/contributors',
       method: 'GET',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
-        'User-Agent': owner.name,
+        'User-Agent': orgname,
         // Uncomment this line to make GET requests from within the site (not with Postman)
         // 'Authorization': `token ${req.body.token}`
         'Authorization': 'token ' + token
@@ -40,107 +39,59 @@ exports.updateStats = function(req, res) {
         console.error(error);
       } else {
         console.log('success in getStats');
-        callback(owner, repo, stats);
+        callback(orgname, repo, stats);
       }
     });
   };
-    
-  // callback function - will delete old stats, then add new stats to our database
-  var updateStatsInDb = function(owner, repo, stats) {
-    var statsArray = JSON.parse(stats);
-    
-    if (statsArray.length !== undefined) {
-      var dbTimestamp = pgp.as.date(new Date());
-      db.tx(function(t) {
-              
-        var deleteStatsQuery = t.any(
-          'DELETE from $1~' +
-          'WHERE $2~=$3',
-          ['stats', 'repo_id', repo.id]);      
-        
-        var insertStatsAndJoinsQuery = statsArray.map(function(stat) {
-          
-          return (t.any(
-            'INSERT INTO $1~ ($2~, $3~, $4~, $5~, $6~) ' +
-            'VALUES ($7, $8, $9, $10, $11) ' +
-            'RETURNING $12~',
-            ['stats', 'updated_ga', 'repo_id', 'author_id', 'total', 'weeks',
-            dbTimestamp, repo.id, stat.author.id, stat.total, JSON.stringify(stat.weeks), 'id_ga'])
-            .then(data => {
-              var statId = data[0].id_ga;
-              console.log('statId', statId);
-              // the query will need to save values in both the user_id column and org_id column
-              // but only one will have an actual id in it
-              
-              // the column we are saving a real id to 
-              var validIdCol = owner.type + '_id';
-              // the column we are saving a dummy id to
-              var nullIdCol;
-              
-              if (owner.type === 'user') {
-                nullIdCol = 'org_id';
-              } else {
-                nullIdCol = 'user_id';
-              }
-              
-              console.log('validIdCol: ', validIdCol);
-              console.log('owner.id: ', owner.id);
-              console.log('nullIdCol: ', nullIdCol);
-              
-              // START HERE - this insert is failing, with this showing in the console:
-              
-              /*
-              statId 639
-              validIdCol:  user_id
-              owner.id:  15864056
-              nullIdCol:  org_id
-              Unhandled rejection error: insert or update on table "owners_stats" violates foreign key constraint "stats_owners_stats"
-              */
-              
-              db.any(
-                'INSERT INTO $1~ ($2~, $3~, $4~, $5~) ' +
-                'VALUES ($6, $7, $8, $9) ' +
-                'RETURNING *',
-                ['owners_stats', 'created_ga', 'stats_id_ga', validIdCol, nullIdCol,
-                dbTimestamp, statId, owner.id, 0]);
-            })
-          );
+  
+  // ** BUSINESS LOGIC FOR PATCH REQUEST **
+  
+  // retrieve this user's username
+  db.one('SELECT * ' +
+    'FROM $2~ ' +
+    'WHERE $3~=$4',
+    ['username', 'users', 'id', queryId])
+  .then(user => {
+    console.log('user', user);
+    // select all of this user's repos
+    db.any('SELECT r.id, r.updated_ga, r.created_at, r.watchers_count, r.stargazers_count, r.forks_count ' + 
+      'FROM users_repos ur ' +
+      'INNER JOIN repos r ' + 
+      'ON r.id = ur.repo_id ' + 
+      'WHERE ur.user_id=$1', 
+      [queryId])
+      .then(repos => {
+        console.log(repos);
+        // find the org (if any) associated with each repo
+        db.tx(function(t) {
+          var queries = repos.map(function (repo) {
+            return t.oneOrNone('SELECT o.id, o.updated_ga, o.orgname, o.avatar_url, o.followers, o.following, o.score ' + 
+              'FROM orgs_repos ogr ' +
+              'INNER JOIN orgs o ' + 
+              'ON o.id = ogr.repo_id ' + 
+              'WHERE ogr.repo_id=$1', 
+              [repo.id]);
+          });
+          return t.batch(queries);
+        })
+        .then(orgs => {
+          console.log('orgs[0]', orgs[0]);
+          res.send('success');
+        })
+        .catch(error => {
+          console.error('error: ', error);
+          res.send(500);
         });
-        
-        // return t.batch(insertStatsAndJoinsQuery);
-        var queryArray = [deleteJoinsQuery, deleteStatsQuery].concat(insertStatsAndJoinsQuery);
-      })
-      .then(data => {
-        console.log('success in updateStatsInDb');
+        // for each org / repo pair, get the stats from GitHub
       })
       .catch(error => {
-        console.error('Error updating stats in db: ', error);
-      });
-    }
-  };
-    
-  // CALL HELPER FUNCTIONS
-    
-  db.any(
-    'SELECT * FROM $1~ ' +
-    'WHERE $1~.$2~=$3',
-    ['repos', 'owner', owner.name])
-    .then(repos => {
-      // for each repo:
-        // make a GET request to GitHub at /repos/:owner/:repo/stats/contributors
-        // delete existing stats and joins for the repo from our database
-        // add stats and joins for the repo to our database
-      repos.forEach((repo, index) => {
-        getStatsFromGitHub(owner, repo, updateStatsInDb);
-      });
-    })
-    .then(data => {
-      res.send('success');
-    })
-    .catch(error => {
-      console.error('Error selecting repos: ', error);
-      // res.status(500).send('Error selecting repos');
-    });
-
+        res.send(500);
+        console.error('error: ', error);
+      });  
+  })
+  .catch(error => {
+    res.send(500);
+    console.error('error: ', error);
+  });
 
 };
