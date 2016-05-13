7810025623ca08ad1226d275c79846e496b8edb9
adamrgisom <adamrgisom@gmail.com>
2016-05-12 19:10:55 -0700

    Pare down chart considerably, shows 2 bars with data

--- client/components/commitChart.js
+++ client/components/commitChart.js
@@ -1,71 +1,33 @@
-//@TODO: decide what to do in case of a tie! The obvious thing is to opacity-1/2 (or 1/4) 2 exactly overlapping rects
+//@TODO: decide what to do in case of a tie!
+// The obvious thing is to opacity-1/2 (or 1/4) 2 exactly overlapping rects
 
 module.exports = (data) => {
 
-  var recentActivity = data.owner.slice(data.owner.length - 13, data.owner.length);
-  var beginningOfActivityIndex = 0;
-  while (recentActivity[beginningOfActivityIndex] === 0) {
-    beginningOfActivityIndex++;
-  }
-  // show only the last 13 weeks of data, or last N weeks if commit activity is only in last N weeks,
-  // but if all last 13 weeks had no data, just show a flat graph of 13 weeks of 0 activity
-  recentActivity = recentActivity.slice(beginningOfActivityIndex, recentActivity.length);
+  // EXAMPLE OF DATA COMING IN:
+  data = [24, 12];
 
-  // generate time axis -- start with week
-  const generateTimeAxisTicks = () => {
-    var now = new Date();
-    var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+  var mostCommits = Math.max(...data);
 
-    var timeAxis = [], lastSunday, timeString;
-    if (recentActivity.length > 0) {
-      timeAxis.push((today.getMonth()+1) + '/' + today.getDate());
-
-      for (var i = 1; i < recentActivity.length; i++) {
-          var lastSunday = i === 1 ? new Date(today.setDate(today.getDate()-today.getDay())) : new Date(lastSunday - 604800000);
-          timeString = (lastSunday.getMonth()+1) + '/' + lastSunday.getDate();
-          timeAxis.push(timeString);
-      }
-    }
-    return timeAxis.reverse();
-  };
-  var timeAxis = generateTimeAxisTicks();
+  // HARDCODED FOR NOW
+  var users = ['@adamrgisom', '@msmith9393'];
 
   /*************
-   Some data 'processing'
-  *************/
-  // calculate mostCommits
-  var mostCommits = Math.max(...recentActivity);
-
-  // get sorted data, SET TO SORTED RECENT ACTIVITY
-  var sortedRecentActivity = recentActivity;
-  // loop through users (each has a recentActivity)
-    // per week, sort in ascending order by...
-    // create array of tuples where tuple is [commitsByUser[i], i] where i indicates which user
-    // sort with function(a,b){ return a[0] > b[0]; }
-
-  /*************
-   Actual D3: the setup
+   The setup
   *************/
   // set dimensions
   var pad = 30;
   var w = 600 - 2*pad;
   var h = 360 - 2*pad;
-  console.log('timeAxis.length')
-  var barWidth = Math.floor((w-2*pad)/timeAxis.length);
+  var barWidth = Math.floor((w-2*pad)/users.length);
 
-  // create container / svg
-  // if (!d3.select("#commit-charts svg")) {
-  //   d3.select("#commit-charts")
-  //     .append("svg")
-  //       .attr("width", w)
-  //       .attr("height", h);
-  // }
   var svg = d3.select("#commit-charts svg");
+
+  // blank out the svg to re-render it
   svg.selectAll('*').remove();
 
   // if there is NO recent activity, don't draw a graph
   // just write text saying 'no recent activity' in an ellipse (?)
-  if (recentActivity.length === 0) {
+  if (data.length === 0) {
     svg.append('text')
       .text('no recent activity on this repo!')
       .attr('x', w/2)
@@ -77,7 +39,7 @@ module.exports = (data) => {
 
   // set the scales
   var xScale = d3.scale.ordinal()
-    .domain(timeAxis)
+    .domain(users)
     .rangeRoundBands( [pad*2, w] );
   var yScale = d3.scale.linear()
     .domain(
@@ -105,78 +67,35 @@ module.exports = (data) => {
       stroke: '#333'
     })
     .call(yAxis);
+
   // declare colors array, maximum 5
   // Note that if there were only 5 colors, that would mean only up to 5 individuals could get compared.
-  var colors = [
-    'lightgreen', 'steelblue', 'red', 'gray', 'black'
-  ];
+  var colors = [ 'lightgreen', 'steelblue'];
 
   // add the bars in the bar graph
-  // HARDCODE LENGTH TO BE 1, IT SHOULD BE USERS.LENGTH
-  var length = 1;
-  var skinnyBarWidth = barWidth/length;
-  var whichData = length > 2 ? recentActivity : sortedRecentActivity;
-
   var g = svg.selectAll(".bars")
-    .data(whichData)
+    .data(data)
     .enter()
       .append("g")
-      for (var j = 0; j < length; j++) {
+      for (var j = 0; j < 2; j++) {
         g.append("rect")
           .attr('fill', (d, i) => colors[i])
-          .attr('x', (d, i) => {
-            return length > 2 ? xScale(timeAxis[i]) + (j * skinnyBarWidth) : xScale(timeAxis[i])
-           })
+          .attr('x', (d, i) => xScale(users[i]))
           .attr('y', (d) => yScale(d))
-          .attr('width', () => {
-            return length > 2 ? skinnyBarWidth : barWidth
-           })
+          .attr('width', () => barWidth)
           .attr('height', (d) => yScale(0) - yScale(d))
       }
 
   g.append("g")
-    for (var j = 0; j < length; j++) {
+    for (var j = 0; j < users; j++) {
       g.append("rect")
         .attr('fill', (d, i) => colors[i])
-        .attr('x', (d, i) => {
-          return length > 2 ? xScale(timeAxis[i]) + (j * skinnyBarWidth) : xScale(timeAxis[i])
-         })
+        .attr('x', (d, i) => xScale(users[i]))
         .attr('y', (d) => yScale(d))
-        .attr('width', () => {
-          return length > 2 ? skinnyBarWidth : barWidth
-         })
+        .attr('width', () => barWidth)
         .attr('height', (d) => yScale(0) - yScale(d))
     }
 
-};
-
-
-
-
-  //   // Store each user's data for each day as a tuple of [id, commits]
-  //   // The tuple is because we'll need to keep track of the id/user after we sort
-  //   var data = [], sortedData, day, tuple;
-  //   for (i = 0; i < 7; i++) {
-  //     day = [];
-  //     for (j = 0; j < users.length; j++) {
-  //       tuple = [];
-  //       tuple.push(j); // used as the index
-  //       tuple.push(users[j][i]); // a specific user's # of commits on a specific day
-  //       day.push(tuple); // the day array now has commit data for each of the users (for that day)
-  //     }
-  //     data.push(day); // data will have 7 'day' arrays, which will each have n 'tuples' where n = # of users
-  //   }
-  //   // Sort data so that for each day the users' tuples are sorted in descending order (most-commits-first)
-  //   sortedData = JSON.parse(JSON.stringify(data)); // for some reason .slice seems to be changing original after sort
-  //   for (i = 0; i < 7; i++) {
-  //     sortedData[i].sort(function(a,b) { return a[1] < b[1]; });
-  //   }
-
-  //   /*************
-  //    D3 continued: the part dealing with the data
-  //   *************/
-
-  //
   //   // add text labels for # of commits (when greater than 0)
   //   svg.append('g')
   //     .selectAll('text')
@@ -225,4 +144,4 @@ module.exports = (data) => {
   //       .text(() => usernames[j]);
   //   }
 
-  // });
+};
