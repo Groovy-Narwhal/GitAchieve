51a55e040b39c1d5258090fc5b3645e42e82263a
Alex Nitta <a.j.nitta@gmail.com>
2016-05-12 20:08:18 -0700

    (feat) Start working on commitController

---
+++ server/controllers/commitController.js
@@ -0,0 +1,15 @@
+const request = require('request');
+const db = require('../db/database.js').db;
+const pgp = require('../db/database.js').pgp;
+const token = require('../config/github.config').token;
+
+
+// GET at '/api/v1/users/:id/commits'
+exports.retrieveCommits = function(req, res) {
+
+};
+
+// PATCH at '/api/v1/users/:id/commits'
+exports.updateCommits = function(req, res) {
+
+};
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -3,6 +3,8 @@ const userController = require('./../controllers/userController.js');
 const repoController = require('./../controllers/repoController.js');
 const friendController = require('./../controllers/friendController.js');
 const statController = require('./../controllers/statController.js');
+const commitController = require('./../controllers/commitController.js');
+
 
 // the following routes start from /api/v1/users
 
@@ -28,6 +30,10 @@ userRouter.route('/:id/friends')
 userRouter.route('/:id/stats')
   .get(statController.retrieveStats)
   .patch(statController.updateStats);
+  
+userRouter.route('/:id/commits')
+  // .get(commitController.retrieveCommits)
+  .patch(commitController.updateCommits);  
 
 // userRouter.route('/:id/achievements')
 //   .get(userController.retrieveAchievements)
