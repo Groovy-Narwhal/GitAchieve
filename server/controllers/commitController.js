4251df364874dce64dd96e1d7ae77bf0757a2eb4
Alex Nitta <a.j.nitta@gmail.com>
2016-05-13 12:49:46 -0700

    (feat) Update schema and work on commitController logic

--- server/controllers/commitController.js
+++ server/controllers/commitController.js
@@ -9,9 +9,25 @@ exports.retrieveCommits = function(req, res) {
 
 };
 
+
 // PATCH at '/api/v1/users/:id/commits'
 exports.updateCommits = function(req, res) {
   var queryId = req.params.id;
+  var dbTimestamp = pgp.as.date(new Date());
+  var entityMap = {
+    "&": "&amp;",
+    "<": "&lt;",
+    ">": "&gt;",
+    '"': '&quot;',
+    "'": '&#39;',
+    "/": '&#x2F;'
+  };
+
+  var escapeHtml = function(string) {
+    return String(string).replace(/[&<>"'\/]/g, function (s) {
+      return entityMap[s];
+    });
+  }
   
   // get the user for this queryId
   db.one('SELECT * from $1~ ' +
@@ -59,45 +75,74 @@ exports.updateCommits = function(req, res) {
                 rp(options)
                   .then(commits => {
                     db.tx(t => {
+                      // insert each commit's author as a user if they don't exist in users table
                       var queries = commits.map(commit => {
-                        
-                        // START HERE - do an upsert for each commit using the SHA as 
-                        // a unique id
-                        
-                        // return t.any('INSERT INTO commits')
-                      
-                      
+                        return t.any ('INSERT INTO $1~ as $2~ ($3~, $4~, $5~, $6~, $7~, $8~) ' +
+                          'VALUES ($9, $10, $11, $12, $13, $14) ' +
+                          'ON CONFLICT ($3~) ' +
+                          'DO NOTHING',
+                          ['users', 'u', 'id', 'created_ga', 'username', 'signed_up', 'email', 'avatar_url',
+                          commit.author.id, dbTimestamp, commit.author.login, false, commit.commit.author.email, commit.author.avatar_url]);
+                      });
+                      return t.batch(queries);
+                    })  
+                    .then(users => {
+                      db.tx(t => {
+
+                        // insert each commit as long as the SHA doesn't exist in commits table
+                        var queries = commits.map((commit, index, commits) => {
+                          var message = ''.concat(escapeHtml(commit.commit.message));
+                          var date = pgp.as.date(new Date(commit.commit.author.date));
+                          
+                          // return t.any('INSERT INTO $1~ AS $2~ ($3~, $4~, $5~, $6~, $7~) ' +
+                          //   'VALUES ($8, $9, $10, $11, $12) ' +
+                          //   'ON CONFLICT ($6~) ' + 
+                          //   'DO UPDATE SET ($4~, $5~, $6~, $7~) = ($9, $10, $11, $12) ' +
+                          //   'WHERE $2~.$3~ = $8',
+                          //   ['commits', 'c', 'sha', 'updated_ga', 'date', 'user_id', 'commit_message',
+                          //   commit.sha, dbTimestamp, date, commit.author.id, message]);
+                          
+                          return t.any('INSERT INTO $1~ AS $2~ ($3~, $4~, $5~, $6~, $7~) ' +
+                            'VALUES ($8, $9, $10, $11, $12)',
+                            ['commits', 'c', 'sha', 'updated_ga', 'date', 'user_id', 'commit_message',
+                            commit.sha, dbTimestamp, date, commit.author.id, message]);                          
+                          
+                        });
+                        return t.batch(queries);
                       })
+                      .then(users => {
+                        console.log('success adding commits: ', error);
+                      })
+                      .catch(error => {
+                        console.error('Error adding commits: ', error);
+                        res.status(500).send;
+                      }); 
                     })
-                    
-                    
-                    
+                    .catch(error => {
+                      console.error('Error adding users: ', error);
+                      res.status(500).send;
+                    }); 
                   })
                   .catch(error => {
-                    console.error('Error in GET request');
+                    console.error('Error in GET request: ', error);
                     res.status(500).send;
                   }); 
               })
               .catch(error => {
-                console.error('Error selecting repos');
+                console.error('Error selecting repos: ', error);
                 res.status(500).send;
               });            
           });
-          
-          res.send('success');
-          // db.tx(t => {
-          //   var queries = repos.map(repo => {
-          //     return 
-          //   })
-          // })
         })
-      .catch(error => {
-        console.error('Error selecting repos');
-        res.status(500).send;
-      });      
+        .catch(error => {
+          console.error('Error selecting orgs: ', error);
+          res.status(500).send;
+        });      
     })
     .catch(error => {
-      console.error('Error selecting user');
+      console.error('Error selecting user: ', error);
       res.status(500).send;
     });
 };
+
+
