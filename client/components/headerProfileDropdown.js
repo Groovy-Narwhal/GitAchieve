06107bd88a693da292d695a8d02252c87b90c83e
Megan Smith <megan@tokaypress.com>
2016-05-14 12:15:03 -0700

    (fix) Fix links to profile and achievements

--- client/components/headerProfileDropdown.js
+++ client/components/headerProfileDropdown.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { browserHistory } from 'react-router';
+import { browserHistory, Link } from 'react-router';
 import actions from './../actions/ActionCreators';
 
 class HeaderUserButton extends Component {
@@ -13,8 +13,8 @@ class HeaderUserButton extends Component {
   render() {
     return (
       <div className="header-profile-dropdown">
-        <div className="header-profile-dropdown-item" onClick={() => (browserHistory.push('/profile'))}>profile</div>
-        <div className="header-profile-dropdown-item" onClick={() => (browserHistory.push('/achievements'))}>achievements</div>
+        <div className="header-profile-dropdown-item" onClick={() => (browserHistory.push(`/${this.props.user.username}/repos`))}>profile</div>
+        <div className="header-profile-dropdown-item" onClick={() => (browserHistory.push(`/${this.props.user.username}/achievements`))}>achievements</div>
         <div className="header-profile-dropdown-item" onClick={this.props.actions.unAuthUser}>signout</div>
       </div>
     )
