735c916ce4ed42eb990256d9d296cf70f0470555
Alex Nitta <a.j.nitta@gmail.com>
2016-05-13 20:39:57 -0700

    (feat) Begin sorting by date in commitStartController

--- server/controllers/commitStartController.js
+++ server/controllers/commitStartController.js
@@ -8,16 +8,30 @@ const rp = require('request-promise');
 // headers must include the 'repoid' and the 'startdate'
 exports.retrieveCompetition = function(req, res) {
   var queryId = req.params.id;
-  var startDate = req.headers.startdate;
-  var repoid = req.headers.repoid;
+  var startDate = new Date (req.headers.startdate);
+  var repoId = req.headers.repoid;
+  
   
   db.any('SELECT sha, updated_ga, date, user_id, commit_message ' +
     'FROM commits_repos cr ' +
-    'INNER JOIN commits c' +
-    'ON cr.repo_id = $1',
-    [repoId])
+    'INNER JOIN commits c ' +
+    'ON c.sha = cr.commit_sha ' +
+    'WHERE cr.repo_id = $1 ' +
+    'AND c.user_id = $2',
+    [repoId, queryId])
     .then(commits => {
-      res.send(commits);
+      var filteredCommits = commits.reduce((filtered, commit) => {
+        if (new Date(commit.date) - startDate > 0) {
+          console.log('startDate', startDate);
+          console.log('commitDate', new Date(commit.date));
+          console.log('--------');
+          filtered.push(commit);
+          return filtered;
+        } else {
+          return filtered;
+        }       
+      }, []);
+      res.send(filteredCommits);
     })
     .catch(error => {
       console.error('Error querying commits: ', error);
@@ -26,7 +40,7 @@ exports.retrieveCompetition = function(req, res) {
 };
 
 var sampleDate = pgp.as.date(new Date());
-console.log(new Date());
+console.log(new Date(new Date() - 6 * 1000 * 1 * 60 * 60 * 24));
 
 // new Date ()
 // Fri May 13 2016 19:26:14 GMT-0700 (PDT)
@@ -35,3 +49,8 @@ console.log(new Date());
 // 'Sat, 14 May 2016 02:23:37 GMT'
 
 console.log(new Date('Sat, 14 May 2016 02:23:37 GMT'));
+
+var d1 = new Date(); //"now"
+var d2 = new Date("2016/05/10")  // some date
+var diff = (d1 - d2)/(1000 * 1 * 60 * 60 * 24);  
+console.log(diff);
