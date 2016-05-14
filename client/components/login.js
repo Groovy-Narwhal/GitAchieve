dbcec93727f68866691a9d23236bdff6efa87dcf
byeo630 <byeo630@gmail.com>
2016-05-14 15:29:38 -0700

    (feat) Contain login elements inside of styled div

--- client/components/login.js
+++ client/components/login.js
@@ -10,10 +10,12 @@ export default class Login extends Component {
   render() {
     return (
       <div>
-        <p>Who's on top of their code game?</p>
-        <p>GitAchieve adds a competitive and fun edge to your engineering teams workflow. Visualize your git statistics against your fellow coders!</p>
-        <a href="/auth/github_oauth">Sign in with GitHub</a>
-        <button onClick={() => browserHistory.push('/about')}>Learn about the team</button>
+        <div className="text-centered data-results-container">
+          <h2 className="font-white">Are you on top of your Git game?</h2>
+          <p className="font-white">GitAchieve adds a competitive and fun edge to your engineering team's workflow. See your Git statistics visualized against your fellow coders!</p>
+          <a href="/auth/github_oauth"><button className="button">Sign in with GitHub</button></a>
+          <button onClick={() => browserHistory.push('/about')}>Learn about the team</button>
+        </div>
       </div>
     )
   }
