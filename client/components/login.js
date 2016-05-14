87ffc210353cd22d84b231d0d0c43427d20f2790
byeo630 <byeo630@gmail.com>
2016-05-14 16:16:21 -0700

    (feat) Add initial styling for login page

--- client/components/login.js
+++ client/components/login.js
@@ -11,10 +11,13 @@ export default class Login extends Component {
     return (
       <div>
         <div className="text-centered data-results-container">
-          <h2 className="font-white">Are you on top of your Git game?</h2>
-          <p className="font-white">GitAchieve adds a competitive and fun edge to your engineering team's workflow. See your Git statistics visualized against your fellow coders!</p>
-          <a href="/auth/github_oauth"><button className="button">Sign in with GitHub</button></a>
-          <button onClick={() => browserHistory.push('/about')}>Learn about the team</button>
+          <div className="spacer-10px"/>
+          <h1 className="font-gray">Are you on top of your Git game?</h1>
+          <p className="font-gray">Add a competitive and fun edge to your engineering team's workflow. See your Git statistics visualized against your fellow coders!</p>
+          <div className="spacer-20px"/>
+          <a href="/auth/github_oauth"><button className="button signin">Sign in with GitHub <img src="static/assets/GitHub-Mark-Light-32px.png"/></button></a>
+          <div className="spacer-10px"/>
+          <button onClick={() => browserHistory.push('/about')} className="button">Learn about the team</button>
         </div>
       </div>
     )
