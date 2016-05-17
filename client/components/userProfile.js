7e7cb785f6ed65286c3fe1e531b23920fdc48733
byeo630 <byeo630@gmail.com>
2016-05-16 19:53:02 -0700

    (feat) Format user profile's friends and events

--- client/components/userProfile.js
+++ client/components/userProfile.js
@@ -27,10 +27,10 @@ class UserProfile extends Component {
     this.props.actions.searchUserEvents([]);
   }
   componentWillMount() {
-    this.getFriends();
+    this.fetchFriends();
     this.fetchEvents.call(this)
   }
-  getFriends() {
+  fetchFriends() {
     axios.get(`http://127.0.0.1:8000/api/v1/users/${this.props.user.id}/friends`, this.state.options)
       .then(data => this.setState({friends: data.data}))
   }
@@ -46,86 +46,82 @@ class UserProfile extends Component {
     }
   }
   eventTypeFilter(event) {
-     switch (event.type) {
-       case 'PushEvent':
-         return (
-           <div className="data-result-container event-commits">
-             <strong>{event.payload.commits.length} commits</strong>
-           </div>
-           );
-       case 'PullRequestEvent':
-         return (
-           <div className="data-result-container">
-             <strong>action: {event.payload.action}</strong>
-             <p>number of commits: {event.payload.pull_request.commits}</p>
-             <p>number of changed files: {event.payload.pull_request.changed_files}</p>
-             <p>number of additions: {event.payload.pull_request.additions}</p>
-             <p>number of deletions: {event.payload.pull_request.deletions}</p>
-           </div>
+    switch (event.type) {
+      case 'PushEvent':
+        return (
+          <div className="data-result-container event-commits">
+            <img src="./../static/assets/circle-star-1-1.svg" height="15px" width="15px"/>
+            <h3 className="event-title">You pushed {event.payload.commits.length} commits to {event.repo.name}</h3>
+          </div>
          );
-       case 'GollumEvent':
-         return (<div></div>);
-       case 'IssueCommentEvent':
-         return (<div></div>);
-       case 'DeleteEvent':
-         return (<div></div>);
-       default:
-         return (<div></div>);
-     }
-   }
-   renderEvents() {
-      if (this.props.searchUserEvents.length > 0) {
-        return this.props.searchUserEvents.map((event, index) => {
-          if (event.type === 'PushEvent' || event.type === 'PullRequestEvent') {
-            return (
-              <div key={index} className="search-result-container" >
-                <h3 className="event-title">{event.type}</h3>
-                <span className="event-title"> at </span>
-                <h3 className="event-title">{event.repo.name}</h3>
-                <span>{event.created_at}</span>
-                {this.eventTypeFilter(event)}
-              </div>
-            )
-          }
-        });
-      } else {
-        return this.state.userEvents.map((event, index) => {
-          if (event.type === 'PushEvent' || event.type === 'PullRequestEvent') {
-            return (
-              <div key={index} className="search-result-container" >
-                <span className={classNames("font-lightest-gray font-weight-light fonts-size-regular")}>{moment(new Date(event.created_at)).fromNow()}</span>
-                <h3 className="event-title">{event.type}</h3>
-                <span className="event-title"> at </span>
-                <h3 className="event-title">{event.repo.name}</h3>
-                {this.eventTypeFilter(event)}
-              </div>
-            )
-          }
-        });
-      }
-   }
-   renderFriends() {
-    if (this.state.friends.length !== 0) {
-      return this.state.friends.map(friend => (
-        <div key={friend.id}><p><Link to={`/${friend.username}/profile`}>{friend.username}</Link></p></div>
-      ))
+      case 'PullRequestEvent':
+        return (
+          <div className="data-result-container">
+            <img src="./../static/assets/git-pull-request-1-2.png" />
+            <h3>You {event.payload.action} a pull-request</h3>
+            <p>number of commits: {event.payload.pull_request.commits}</p>
+            <p>number of changed files: {event.payload.pull_request.changed_files}</p>
+            <p>number of additions: {event.payload.pull_request.additions}</p>
+            <p>number of deletions: {event.payload.pull_request.deletions}</p>
+          </div>
+        );
+      default:
+        return (<div></div>);
     }
-   }
-   render() {
-     return (
-       <div className="data-results-container">
-         <img src={this.props.user.avatar_url} className="user-avatar-1" />
-         <h2>{this.props.user.username}</h2>
-         <div>
-           <h2>Friends</h2>
-           {this.renderFriends()}
-         </div>
-         <div id="search-results-container">
-           {this.renderEvents()}
-         </div>
-       </div>
-     )
-   }
+  }
+  renderEvents() {
+    if (this.props.searchUserEvents.length > 0) {
+      return this.props.searchUserEvents.map((event, index) => {
+        if (event.type === 'PushEvent' || event.type === 'PullRequestEvent') {
+          return (
+            <div key={index} className="search-result-container" >
+              <h3 className="event-title">{event.type}</h3>
+              <span className="event-title"> at </span>
+              <h3 className="event-title">{event.repo.name}</h3>
+              <span>{event.created_at}</span>
+              {this.eventTypeFilter(event)}
+            </div>
+          )
+        }
+      });
+    } else {
+      return this.state.userEvents.map((event, index) => {
+        if (event.type === 'PushEvent' || event.type === 'PullRequestEvent') {
+          return (
+            <div key={index} className="search-result-container" >
+              <span className={classNames("font-medium-gray font-weight-light fonts-size-regular")}>{moment(new Date(event.created_at)).fromNow()}</span>
+              <div className="spacer-5px"/>
+              {this.eventTypeFilter(event)}
+            </div>
+          )
+        }
+      });
+    }
+  }
+  renderFriends() {
+  if (this.state.friends.length !== 0) {
+    return this.state.friends.map(friend => (
+      <div key={friend.id} className="data-result-container"><p><Link to={`/${friend.username}/profile`}>{friend.username}</Link></p></div>
+    ))
+  }
+  }
+  render() {
+    return (
+      <div className="data-results-container">
+        <div className="data-result-container">
+          <img src={this.props.user.avatar_url} className="user-avatar-1" />
+          <h2 className="profile-username">{this.props.user.username}</h2>
+        </div>
+        <div className="data-result-container">
+          <h2>Friends</h2>
+          {this.renderFriends()}
+        </div>
+        <div className="data-results-container full-width">
+          {this.renderEvents()}
+        </div>
+      </div>
+    )
+  }
 }
 /*
 {event.payload.commits.map((commit, index) => (
