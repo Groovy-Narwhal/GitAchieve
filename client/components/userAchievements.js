e4067f6ce7f916008fda7bcfd345fb28ba853678
byeo630 <byeo630@gmail.com>
2016-05-17 19:25:06 -0700

    (feat) Display trophies on achievements page

--- client/components/userAchievements.js
+++ client/components/userAchievements.js
@@ -10,9 +10,16 @@ class DashBoard extends Component {
   }
 
   render() {
+    var achievements = [
+    {title:'won a competition', img_url: '/static/assets/trophies/trophy-1-1.png'}, 
+    {title:'3 game winning streak', img_url: '/static/assets/trophies/trophy-1-1.png'}, 
+    {title:'100 commits', img_url: '/static/assets/trophies/trophy-2-1.png'}, 
+    {title:'200 commits', img_url: '/static/assets/trophies/trophy-2-1.png'}
+    ]
     return (
-      <div className="achievements">
+      <div className="data-results-container">
         <div>Achievements</div>
+        {achievements.map((achievement) => (<div><h3>{achievement.title}</h3><img src={achievement.img_url} /></div>))}
         <img src="../static/assets/medal.svg" alt="medal image" height="50px" width="50px" />
       </div>
     )
