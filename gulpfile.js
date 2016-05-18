14f8cef82eaeb25ef77ebea47b3edb0dd6fc3989
Alex Nitta <a.j.nitta@gmail.com>
2016-05-17 19:18:48 -0700

    (feat) Refactor Gulpfile to serve development vs. production files
    Also add linting with eslint and Hack Reactor's style guide

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,37 @@
 var gulp = require('gulp');
 
 // Include Our Plugins
-var jshint = require('gulp-jshint');
+var babel = require('gulp-babel');
+var clean = require('gulp-clean');
+var env = require('gulp-env');
+var eslint = require('gulp-eslint');
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var mocha = require('gulp-mocha');
+var nodemon = require('gulp-nodemon');
 var util = require('gulp-util');
 var shell = require('gulp-shell');
 var webpack = require('webpack-stream');
+var webpackOptions = require('./webpack.config.production.js');
 var mochaPhantomJs = require('gulp-mocha-phantomjs');
+
+var paths = {
+  scripts: ['server/**/*.js']
+};
 
 // Lint Task
 gulp.task('lint', function() {
   return gulp.src(['./client/**/*.js', './server/**/*.js'], {base: '.'})
-    .pipe(jshint({esnext: true}))
-    .pipe(jshint.reporter('default'));
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(eslint({
+      config: './test/hrStyleGuide.json'
+    }))
+    .pipe(eslint.format())
+    .on('error', util.log);
 });
 
 // Test Task
@@ -26,21 +41,41 @@ gulp.task('test', shell.task([
   'jasmine-node test/server/ --junitreport'
 ]));
 
-// Build Task
-gulp.task('build', function() {
-  return gulp.src('client/index.js')
-    .pipe(webpack())
-    .pipe(gulp.dest('dist/'));
+// Clean out previous dist folder
+gulp.task('clean', function () {  
+  return gulp.src('dist', {read: false})
+    .pipe(clean());
 });
 
-// Concatenate & Minify JS Task
-gulp.task('scripts', function() {
-  return gulp.src('server/**/*.js')
-    .pipe(concat('all.js'))
+// Concatenate & Uglify client-side JS
+  // 'clean' must finish before this will start
+gulp.task('build-client', ['clean'], function() {
+  return gulp.src('client/index.js')
+    .pipe(webpack(webpackOptions))
+    .pipe(rename('bundle.js'))
     .pipe(gulp.dest('dist'))
-    .pipe(rename('all.min.js'))
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(rename('bundle.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist'))
+    .on('error', util.log);
+});
+
+// Concatenate & Uglify server-side JS
+  // 'clean' must finish before this will start
+gulp.task('build-server', ['clean'], function() {
+  return gulp.src(paths.scripts)
+    .pipe(babel({
+      presets: ['es2015']
+    }))
+    .pipe(concat('server-all.js'))
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('server-all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
+    .on('error', util.log);
 });
 
 // Watch Files For Changes
@@ -49,9 +84,19 @@ gulp.task('watch', function() {
   // gulp.watch('scss/*.scss', ['sass']);
 });
 
+gulp.task('run', function() {
+  env({
+    file: './server/config/.env.json'
+  });
+  nodemon({
+    script: './server/server.js'
+  });
+});
+
 // Default Task
 gulp.task('default', ['lint', 'test', 'watch']);
 
+gulp.task('build', ['clean', 'build-client', 'build-server']);
 
 // Compile Our Sass // Commented out for now since Sass is not integrated yet.
 // gulp.task('sass', function() {
--- server/server.js
+++ server/server.js
@@ -26,9 +26,10 @@ app.use('/static', express.static(__dirname + '/../client'));
 app.get('/', function(req, res) {
   res.sendFile(path.resolve('./client/index.html'));
 });
-app.get('*', function (req, res){
-  res.sendFile(path.resolve('./client/index.html'))
-})
+
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve('./client/index.html'));
+});
 
 // Run server listening on the local environment
 const port = process.env.PORT || 8000;
